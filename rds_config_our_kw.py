37bf27446f5e950e854a888a6397bdf379bcf225
naveen39 <naveensfdc9@gmail.com>
2018-01-23 12:29:27 +0530

    Update rds_config_our_kw.py

--- rds_config_our_kw.py
+++ rds_config_our_kw.py
@@ -1,6 +1,6 @@
 #config file containing credentials for rds mysql instance
-db_username = "admin"
-db_password = "admin369"
+db_username = "username"
+db_password = "pwd"
 db_name = "our_kw_data" 
 mail_user="urmail id"
 mail_pwd="ur pwd"
